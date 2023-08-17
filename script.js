a28011e876fc1c4d504a4d80843dd2e962465113
GyHuba <gy.huba@gmail.com>
2023-08-17 20:47:24 +0200

    the basic things works

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 const container = document.querySelector('.container');
 const boardDOM = document.querySelector('.board');
+const colorDOM = document.querySelector('#color');
+const sizeDOM = document.querySelector('#size');
+const clearBtn = document.querySelector('.clear-btn');
 
+let color = "black";
+let boardSize = 16;
 
-function buildTheGrid(times) {
-    let gridCellSize = boardDOM.clientWidth / times;
-        for(let i = 0; i < (times * times); i++){
-            let gridCell = document.createElement('div');
-            gridCell.classList.add('grid-cell');
-            gridCell.style = `width : ${gridCellSize}px; height : ${gridCellSize} px`
-            boardDOM.append(gridCell);
-        }
+function buildTheGrid(size) {
+    let gridCellSize = boardDOM.clientWidth / size;
+    for (let i = 0; i < (size * size); i++) {
+        let gridCell = document.createElement('div');
+        gridCell.classList.add(`grid-cell-${i + 1}`);
+        gridCell.style = `width : ${gridCellSize}px; height : ${gridCellSize} px;`
+        boardDOM.append(gridCell);
     }
+}
 
-// function buildTheGrid(times){
-//     for(let i = 0; i < (times*times); i++){
-//         let divDOM = document.createElement("div");
-//         divDOM.classList.add("grid-element");
-//         divDOM.style.width = `${window.innerWidth / times-1}px`;
-//         divDOM.style.height = `${window.innerHeight / times-1}px`;
-//         divDOM.style.border = '1px solid black';
-//         container.append(divDOM);
-//     }
-// }
+function drawing(event){
+    let gridElement = document.querySelector(`.${event.target.className}`);
+    if (gridElement === null || gridElement.className === "board") return
+    else if (gridElement !== null) {
+        gridElement.style.backgroundColor = `${color}`;
+    }
+}
 
+function setNewBoard(){
+    boardDOM.innerHTML = "";
+    buildTheGrid(boardSize);
+}
+
+boardDOM.addEventListener("mousedown", () => {
+    boardDOM.addEventListener("mouseover", drawing)
+});
+
+boardDOM.addEventListener("mouseup", () => {
+    boardDOM.removeEventListener('mouseover', drawing)
+});
+
+colorDOM.addEventListener('change', (e)=>{
+    color = e.target.value
+})
+
+sizeDOM.addEventListener('change', (e)=>{
+    boardSize= e.target.value;
+    setNewBoard();
+})
+
+clearBtn.addEventListener('click', setNewBoard)
 
 buildTheGrid(16);
+console.log(colorDOM)
