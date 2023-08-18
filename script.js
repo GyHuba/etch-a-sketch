a6821eaed457086404216a22d173fd9e137e8f3b
GyHuba <gy.huba@gmail.com>
2023-08-18 11:29:14 +0200

    finished the basic project

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ let color = "black";
 let boardSize = 16;
 
 function buildTheGrid(size) {
-    boardDOM.style = `width : ${html.clientHeight/1.3}px; height : ${html.clientHeight/1.3}px;`;
-    playgroundDOM.style = `width : ${html.clientHeight/1.2}px`;
-    boardDOM.style.height = html.clientWidth/3;
+    boardDOM.style = `width : ${html.clientHeight / 1.3}px; height : ${html.clientHeight / 1.3}px;`;
+    playgroundDOM.style = `width : ${html.clientHeight / 1.2}px`;
+    boardDOM.style.height = html.clientWidth / 3;
     let gridCellSize = boardDOM.clientWidth / size;
     for (let i = 0; i < (size * size); i++) {
         let gridCell = document.createElement('div');
@@ -23,7 +23,7 @@ function buildTheGrid(size) {
     }
 }
 
-function drawing(event){
+function drawing(event) {
     let gridElement = document.querySelector(`.${event.target.className}`);
     if (gridElement === null || gridElement.className === "board") return
     else if (gridElement !== null) {
@@ -31,7 +31,7 @@ function drawing(event){
     }
 }
 
-function setNewBoard(){
+function setNewBoard() {
     boardDOM.innerHTML = "";
     buildTheGrid(boardSize);
 }
@@ -46,13 +46,13 @@ container.addEventListener("mouseup", () => {
     boardDOM.addEventListener("click", drawing)
 });
 
-colorSelectDOM.addEventListener('click', (e)=>{
+colorSelectDOM.addEventListener('click', (e) => {
     console.log(e.target.id)
     color = e.target.id
 })
 
-sizeDOM.addEventListener('change', (e)=>{
-    boardSize= e.target.value;
+sizeDOM.addEventListener('change', (e) => {
+    boardSize = e.target.value;
     setNewBoard();
 })
 
@@ -62,7 +62,7 @@ clearBtn.addEventListener('click', setNewBoard)
 
 window.onload = (event) => {
     buildTheGrid(16);
-    colors.forEach(element =>{
+    colors.forEach(element => {
         element.style.backgroundColor = element.id
-    })  
-  };
+    })
+};
